89c69c9c8d53c5896bc74e9e25996e760f35a7b4
SuperDed34 <maxnozord@gmail.com>
2024-05-12 23:32:51 +0100

    feat: add dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'], // Добавьте '.jsx' здесь
+  },
   plugins: [react()],
 })
