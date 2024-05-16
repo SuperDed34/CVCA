85efc4e56fbef98a1098e9f6f1417aca3b38c57f
SuperDed34 <maxnozord@gmail.com>
2024-05-16 12:10:38 +0100

    fix: fix routing for work folder in builded projec

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   resolve: {
-    extensions: ['.js', '.jsx', '.json'], // Добавьте '.jsx' здесь
+    extensions: ['.js', '.jsx', '.json'],
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/work/**/*', dest: 'work' }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+        work: resolve(__dirname, './src/work/work.html')
+      },
+      output: {
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.facadeModuleId.includes('/work/')) {
+            return 'work/[name].[hash].js'
+          }
+          return '[name].[hash].js'
+        },
+        chunkFileNames: chunkInfo => {
+          if (chunkInfo.facadeModuleId.includes('/work/')) {
+            return 'work/[name].[hash].js'
+          }
+          return '[name].[hash].js'
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name.includes('/work/')) {
+            return 'work/[name].[hash][extname]'
+          }
+          return '[name].[hash][extname]'
+        }
+      }
+    }
+  }
 })
